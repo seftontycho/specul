60d9aac03658ccdba220d26bbb12a8ca6e5fb6bd
Sefton de Pledge <seftontycho@hotmail.co.uk>
2023-03-27 23:25:30 +0100

    updated docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,34 @@
+//! A simple client for the [RCON](https://developer.valvesoftware.com/wiki/Source_RCON_Protocol) protocol.
+//! # Example
+//! ```no_run
+//! use rcon::{Connection, ConnectionBuilder};
+//! use tokio::net::TcpStream;
+//!
+//! #[tokio::main]
+//! async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!    let mut connection = ConnectionBuilder::default()
+//!       .password("password")
+//!      .build(TcpStream::connect("0.0.0.0:27015").await?);
+//!
+//!     connection.authenticate().await?;
+//!
+//!     let response = connection.execute_command("status").await?;
+//!
+//!     println!("{:?}", response);
+//!
+//!   Ok(())
+//! }
+
 use std::io;
 
+use derive_builder::Builder;
 use err_derive::Error;
 use packet::{Packet, PacketType};
 use tokio::io::{AsyncRead, AsyncWrite};
 
 mod packet;
 
+/// An error that can occur when communicating with the server.
 #[derive(Debug, Error)]
 pub enum Error {
     #[error(display = "{}", _0)]
@@ -14,39 +37,40 @@ pub enum Error {
     #[error(display = "authentication failed")]
     Authentication,
 
-    #[error(display = "command exceded maximum length")]
-    CommandLength,
+    #[error(display = "payload size exceeded")]
+    PayloadSize,
 }
 
+/// A specialized [`Result`](std::result::Result) type for RCON operations.
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+/// A connection to a RCON server.
+/// Can be constructed with any type that implements
+/// [`AsyncRead`](tokio::io::AsyncRead) and [`AsyncWrite`](tokio::io::AsyncWrite).
+#[derive(Debug, Builder)]
 pub struct Connection<T> {
     io: T,
     default_packet_id: i32,
+    #[builder(default = "0", setter(skip))]
     current_packet_id: i32,
     max_payload_size: usize,
+    multiple_responses: bool,
 }
 
 impl<T> Connection<T>
 where
     T: Unpin + AsyncRead + AsyncWrite,
 {
-    pub fn builder() -> Builder {
-        Builder {
-            default_packet_id: 0,
-            max_payload_size: 4096 - 10,
-        }
-    }
-
+    /// Authenticates with the server.
     pub async fn authenticate(&mut self, password: &str) -> Result<()> {
-        self.send(PacketType::Auth, password.to_string()).await?;
+        self.send(PacketType::Authentication, password.to_string())
+            .await?;
 
         let packet = loop {
             let packet = self.receive_packet().await;
 
             if let Some(packet) = packet.ok() {
-                if packet.packet_type == PacketType::AuthResponse {
+                if packet.packet_type == PacketType::AuthenticationResponse {
                     break packet;
                 }
             }
@@ -59,25 +83,37 @@ where
         }
     }
 
+    /// Executes a command on the server.
     pub async fn execute_command(&mut self, command: &str) -> Result<Vec<String>> {
         if command.len() > self.max_payload_size {
-            return Err(Error::CommandLength);
+            return Err(Error::PayloadSize);
         }
 
-        self.send(PacketType::ExecCommand, command.to_string())
-            .await?;
+        self.send(PacketType::Message, command.to_string()).await?;
 
         let response = self.recieve().await?;
 
         Ok(response)
     }
 
-    async fn send(&mut self, packet_type: PacketType, payload: String) -> Result<()> {
+    /// Sends a payload to the server.
+    pub async fn send(&mut self, packet_type: PacketType, payload: String) -> Result<()> {
         let packet = packet::Packet::new(self.new_packet_id(), packet_type, payload);
         self.send_packet(packet).await
     }
 
-    async fn recieve(&mut self) -> Result<Vec<String>> {
+    /// Receives payload(s) from the server.
+    pub async fn recieve(&mut self) -> Result<Vec<String>> {
+        if self.multiple_responses {
+            self.recieve_multi_response().await
+        } else {
+            let reponse = self.recieve_single_response().await?;
+            Ok(vec![reponse])
+        }
+    }
+
+    /// Receives multiple payloads from the server.
+    pub async fn recieve_multi_response(&mut self) -> Result<Vec<String>> {
         let mut responses = Vec::new();
 
         loop {
@@ -94,7 +130,8 @@ where
         Ok(responses)
     }
 
-    async fn recieve_single_response(&mut self) -> Result<String> {
+    /// Receives a single payload from the server.
+    pub async fn recieve_single_response(&mut self) -> Result<String> {
         let packet = self.receive_packet().await?;
 
         Ok(packet.payload.into())
@@ -123,35 +160,5 @@ where
             .unwrap_or(self.default_packet_id);
 
         id
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Ord, PartialOrd, Hash)]
-pub struct Builder {
-    default_packet_id: i32,
-    max_payload_size: usize,
-}
-
-impl Builder {
-    pub fn default_packet_id(mut self, id: i32) -> Self {
-        self.default_packet_id = id;
-        self
-    }
-
-    pub fn max_payload_size(mut self, size: usize) -> Self {
-        self.max_payload_size = size;
-        self
-    }
-
-    pub fn build<T>(self, io: T) -> Connection<T>
-    where
-        T: Unpin + AsyncRead + AsyncWrite,
-    {
-        Connection {
-            io,
-            default_packet_id: self.default_packet_id,
-            current_packet_id: self.default_packet_id,
-            max_payload_size: self.max_payload_size,
-        }
     }
 }
